ef27f073607426c65c962ab90f9070d4dc8b1d7c
spencewenski <sferris37@gmail.com>
2015-02-15 13:22:22 -0500

    add docstring to new_category; create required dirs that don't exists in new_category

--- new_category.py
+++ new_category.py
@@ -1,14 +1,20 @@
-import sys
+"""
+Usage: new_category <tag|category> <slug> <name>
+"""
+
+import sys, os.path
 
 tag_or_category = sys.argv[1] if len(sys.argv) > 1 else '';
 slug = sys.argv[2] if len(sys.argv) > 2 else '';
 name = sys.argv[3] if len(sys.argv) > 3 else '';
 
 if slug == '' or name == '':
-  print 'usage: ' + sys.argv[0] + '[tag|category] <slug> <name>'
+  print __doc__
   sys.exit(0)
 
-# write category page
+# create tag/category dir, then create tag/category page
+if not os.path.exists(tag_or_category):
+  os.makedirs(tag_or_category)
 category_page = open(tag_or_category +'/' + slug + '.md', 'w')
 category_page.write('---\n')
 category_page.write('layout: blog_by_' + tag_or_category + '\n')
@@ -16,6 +22,12 @@ category_page.write(tag_or_category + ': ' + slug + '\n')
 category_page.write('permalink: ' + tag_or_category + '/' + slug + '/\n')
 category_page.write('---\n')
 
-# append category to _data/categories
-data_categories = open('_data/categories.csv', 'a') if tag_or_category == 'category' else open('_data/tags.csv', 'a')
+# create _data/categories if it doesn't exist, then append category to the list
+if not os.path.exists('_data'):
+  os.makedirs('_data')
+file_name = '_data/categories.csv' if tag_or_category == 'category' else '_data/tags.csv'
+file_exists = os.path.isfile(file_name)
+data_categories = open(file_name, 'a')
+if not file_exists:
+  data_categories.write('slug,name\n')
 data_categories.write(slug + ',' + name + '\n')
