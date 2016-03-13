e5383cb1274b663ffdfb18ff21586896a857be08
Spencer Ferris <sferris37@gmail.com>
2016-03-13 16:46:45 -0400

    Add a bash script to run new_tag_or_category.py multiple times
    - Use to run or re-run new_tag_or_category.py on all the tags/categories

---
+++ new_tag_or_category.bash
@@ -0,0 +1,21 @@
+#! /bin/bash
+#
+# Script to run new_tag_or_category.py for all of the tags/categories. Use
+# this to update the files for all the tags/categories created by
+# new_tag_or_category.py
+# 
+python new_tag_or_category.py category travel Travel
+python new_tag_or_category.py category programming Programming
+python new_tag_or_category.py category running Running
+python new_tag_or_category.py category travel Travel
+python new_tag_or_category.py category game_design 'Game Design'
+python new_tag_or_category.py tag games Games
+python new_tag_or_category.py tag game_design 'Game Design'
+python new_tag_or_category.py tag winter_running 'Winter Running'
+python new_tag_or_category.py tag summer_2014 'Summer 2014'
+python new_tag_or_category.py tag hackathons Hackathons
+python new_tag_or_category.py tag mhacks MHacks
+python new_tag_or_category.py tag programming Programming
+python new_tag_or_category.py tag training Training
+python new_tag_or_category.py tag game_analysis 'Game Analysis'
+python new_tag_or_category.py tag writing_201 'Writing 201'
