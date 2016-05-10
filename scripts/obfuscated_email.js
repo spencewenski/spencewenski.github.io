8b8d6397cf6e0b228f2894ead13a11477a95191c
Spencer Ferris <sferris37@gmail.com>
2016-05-10 15:39:10 -0500

    Add obfuscated email to footer

---
+++ scripts/obfuscated_email.js
@@ -0,0 +1,12 @@
+// obfuscated email (http://www.addressmunger.com/)
+function AYQvZItKrBgJUXa(element) {
+  var odKWXInjBGXgrRV=["x73","x70","101","x6e","x63","101","114","64","115","x70","x65","x6e","99","x65","119","x65","x6e","115","x6b","105","x2e","99","x6f","109"];
+  var PXzBWaYxTiHmHwR=[""];
+  var flyNyhOkvDsBGAA=["x73","x70","101","110","x63","x65","x72","64","115","112","101","x6e","99","x65","x77","101","x6e","x73","107","x69","x2e","x63","x6f","x6d"];
+  document.write("<a href=\"&#x6d;&#97;&#x69;&#000108;&#x74;&#x6f;&#x3a;");
+  for (i=0; i<odKWXInjBGXgrRV.length; i++) document.write('&#'+odKWXInjBGXgrRV[i]+';');
+  for (i=0; i<PXzBWaYxTiHmHwR.length; i++) document.write(PXzBWaYxTiHmHwR[i]);
+  document.write('" >');
+  for (i=0; i<flyNyhOkvDsBGAA.length; i++) document.write('&#'+flyNyhOkvDsBGAA[i]+';');
+  document.write('</a>');
+}
