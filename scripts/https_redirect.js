51a6efca60235785af3cbccd172f2c28f51cb53d
Spencer Ferris <sferris37@gmail.com>
2016-12-27 18:45:54 -0500

    Revert "Add script to redirect to https version of website."
    This reverts commit 24d1b00d5410090622445cf3d423b977e46ef71b.

--- scripts/https_redirect.js
+++
@@ -1,4 +0,0 @@
-var host = "spencewenski.com";
-if ((host == window.location.host) && (window.location.protocol != 'https:')) {
-  window.location = window.location.toString().replace(/^http:/, "https:");
-}
