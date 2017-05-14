6bf45beb2636c4a3a1a271c573cae89842323186
Spencer Ferris <sferris37@gmail.com>
2017-05-14 03:25:00 -0700

    Remove grouped_timers/docs/html/ directory

--- grouped_timers/docs/html/search/enumvalues_6.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['settings_5ffield_5fprogress_5fstyle',['SETTINGS_FIELD_PROGRESS_STYLE',['../group___settings__fields.html#gga57c89187120d13bde51bc6603efc7c24aa5664053dbff1a0cd3fa34a9917ea6b1',1,'Settings_fields.h']]],
-  ['settings_5ffield_5frepeat_5fstyle',['SETTINGS_FIELD_REPEAT_STYLE',['../group___settings__fields.html#gga57c89187120d13bde51bc6603efc7c24ac0e5c26d487cde829621167b934b9067',1,'Settings_fields.h']]],
-  ['settings_5ffield_5fvibrate_5finterval',['SETTINGS_FIELD_VIBRATE_INTERVAL',['../group___settings__fields.html#gga57c89187120d13bde51bc6603efc7c24a18716ba6ca1aaa72305022ec1d3d0d85',1,'Settings_fields.h']]],
-  ['settings_5ffield_5fvibrate_5fstyle',['SETTINGS_FIELD_VIBRATE_STYLE',['../group___settings__fields.html#gga57c89187120d13bde51bc6603efc7c24a3becf5bbec43fc12910fc49a28749b1d',1,'Settings_fields.h']]],
-  ['settings_5fview',['SETTINGS_VIEW',['../group__view__factory.html#ggaa25d392ad23ad1a337cb262720e0e378a9a8ec55e3d1950e50ce6c4c107abcc58',1,'view_factory.h']]]
-];
