39189e92f1fe13e73727757e732f69761009307d
Spencer Ferris <sferris37@gmail.com>
2017-09-26 14:42:33 -0400

    Remove https redirect script.
    The https redirect script doesn't actually make the website more secure.
    Actually, it's possibly less secure, because a MITM attack could
    redirect to a different https site, which would mislead the user into
    thinking they had a secure connection to my site while actually having a
    secure connection to different site. Fix this by removing the redirect
    script.

--- scripts/https_redirect.js
+++
@@ -1,4 +0,0 @@
-var host = "spencewenski.com";
-if ((window.location.host == host) && (window.location.protocol != "https:")) {
-	window.location = window.location.toString().replace(/^http:/, "https:");
-}
